65e79dd1d35438bf9ded72b4e9f5b423c6e6590b
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-05-07 09:29:20 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://yt-chat-application.vercel.app/"
+export const BASE_URL="https://yt-chat-application-jn73.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
